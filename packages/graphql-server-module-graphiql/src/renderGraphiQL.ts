6a53b956efc4bc4229ffc6410b90d5c894d0f35f
Markus Hoesel <markus.hoesel@allaboutapps.at>
2017-03-11 11:47:19 +0100

    Pointing graphiql cdn entries to hoeselm.at in order to fetch graphiql with auth token option

--- packages/graphql-server-module-graphiql/src/renderGraphiQL.ts
+++ packages/graphql-server-module-graphiql/src/renderGraphiQL.ts
@@ -61,11 +61,11 @@ export function renderGraphiQL(data: GraphiQLData): string {
       width: 100%;
     }
   </style>
-  <link href="//cdn.jsdelivr.net/graphiql/${GRAPHIQL_VERSION}/graphiql.css" rel="stylesheet" />
+  <link href="//www.hoeselm.at/graphiql-hoeselm/graphiql-hoeselm.css" rel="stylesheet" />
   <script src="//cdn.jsdelivr.net/fetch/0.9.0/fetch.min.js"></script>
   <script src="//cdn.jsdelivr.net/react/15.0.0/react.min.js"></script>
   <script src="//cdn.jsdelivr.net/react/15.0.0/react-dom.min.js"></script>
-  <script src="//cdn.jsdelivr.net/graphiql/${GRAPHIQL_VERSION}/graphiql.min.js"></script>
+  <script src="//www.hoeselm.at/graphiql-hoeselm/graphiql-hoeselm.js"></script>
 </head>
 <body>
   <script>
